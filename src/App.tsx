8c376435cec306e0f968924d7383af4d581e1ab3
mayurlalwani <lalwanimayur06@gmail.com>
2023-03-23 10:09:37 +0530

    convert scss to css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.scss";
+import "./index.css";
 import Main from "./pages";
 import AboutUs from "./pages/about-us/AboutUs";
 
