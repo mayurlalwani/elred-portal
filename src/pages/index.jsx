7f59ae2245faee57c92208d75826cbef509e209b
mayurlalwani <lalwanimayur06@gmail.com>
2023-03-23 13:26:57 +0530

    fix issue in routing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutUs from "./about-us/AboutUs";
+import Layout from "./Layout";
 const Main = () => {
-  return <AboutUs />;
+  return <Layout></Layout>;
 };
 export default Main;
